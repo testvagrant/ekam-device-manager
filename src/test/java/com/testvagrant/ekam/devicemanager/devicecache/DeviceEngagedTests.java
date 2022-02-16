d1d1917fce7e6b347c0ba9d0eb7406dd26bb2cd7
Prajwalgn-07 <prajwalg@testvagrant.com>
2022-02-16 13:47:06 +0530

    DeviceNotFoundException Final Fix --------------------------------- * Changed Logic to provide random devices when the device lock is disabled

--- src/test/java/com/testvagrant/ekam/devicemanager/devicecache/DeviceEngagedTests.java
+++ src/test/java/com/testvagrant/ekam/devicemanager/devicecache/DeviceEngagedTests.java
@@ -3,13 +3,17 @@ package com.testvagrant.ekam.devicemanager.devicecache;
 import com.testvagrant.ekam.devicemanager.DeviceCache;
 import com.testvagrant.ekam.devicemanager.DeviceFiltersManager;
 import com.testvagrant.ekam.devicemanager.DeviceManager;
+import com.testvagrant.ekam.devicemanager.exceptions.DeviceEngagedException;
+import com.testvagrant.ekam.devicemanager.exceptions.NoSuchDeviceException;
 import com.testvagrant.ekam.devicemanager.models.DeviceFilter;
 import com.testvagrant.ekam.devicemanager.models.DeviceFilters;
 import com.testvagrant.ekam.devicemanager.models.EkamSupportedPlatforms;
 import com.testvagrant.ekam.devicemanager.models.TargetDetails;
 import org.junit.jupiter.api.Assertions;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 import java.util.function.Predicate;
 import org.junit.jupiter.api.Test;
 
@@ -17,60 +21,95 @@ public class DeviceEngagedTests {
 
     @Test
     public void shouldGiveDifferentDevicesWhenTheDeviceLockIsEnabled(){
-        TargetDetails pixel3a=
-                TargetDetails.builder()
-                        .platform(EkamSupportedPlatforms.ANDROID)
-                        .name("Google Pixel 3a")
-                        .udid("12")
-                        .build();
-        TargetDetails pixel3b =
-                TargetDetails.builder()
-                        .platform(EkamSupportedPlatforms.ANDROID)
-                        .name("Google Pixel 3b")
-                        .udid("12")
-                        .build();
-
-        List<TargetDetails>targets=List.of(pixel3a,pixel3b);
+        List<TargetDetails> targets = new ArrayList<>();
+        for(int i=0;i<2;i++)
+            targets.add(getTargetDetails());
 
         DeviceCache deviceCache=new DeviceCache(targets);
-
-        Predicate<TargetDetails> predicate = getTargetDetailsPredicate("Google Pixel 3", "android");
+        Predicate<TargetDetails> predicate = getTargetDetailsPredicate( "Google","android");
 
         TargetDetails availableDevice = new DeviceManager(deviceCache).getAvailableDevice(predicate, true);
         TargetDetails availableDevice1 = new DeviceManager(deviceCache).getAvailableDevice(predicate, true);
-        TargetDetails availableDevice2 = new DeviceManager(deviceCache).getAvailableDevice(predicate, true);
 
         Assertions.assertNotEquals(availableDevice.getName(),availableDevice1.getName());
     }
+
     @Test
-    public void shouldGiveSameDeviceWhenTheDeviceLockIsDisabled(){
-        TargetDetails pixel3c=
-                TargetDetails.builder()
-                        .platform(EkamSupportedPlatforms.ANDROID)
-                        .name("Google Pixel 3c")
-                        .udid("12")
-                        .build();
-
-        TargetDetails pixel3b =
-                TargetDetails.builder()
-                        .platform(EkamSupportedPlatforms.ANDROID)
-                        .name("Google Pixel 3b")
-                        .udid("12")
-                        .build();
-
-        List<TargetDetails>targets=List.of(pixel3c,pixel3b);
+    public void shouldGiveRandomDevicesWhenTheDeviceLockIsDisabled(){
+        List<TargetDetails> targets = new ArrayList<>();
+        for(int i=0;i<10;i++)
+            targets.add(getTargetDetails());
 
         DeviceCache deviceCache=new DeviceCache(targets);
-
-        Predicate<TargetDetails> predicate = getTargetDetailsPredicate("Google Pixel 3", "android");
+        Predicate<TargetDetails> predicate = getTargetDetailsPredicate( "android");
 
         TargetDetails availableDevice = new DeviceManager(deviceCache).getAvailableDevice(predicate, false);
         TargetDetails availableDevice1 = new DeviceManager(deviceCache).getAvailableDevice(predicate, false);
-        TargetDetails availableDevice2 = new DeviceManager(deviceCache).getAvailableDevice(predicate, false);
 
-        Assertions.assertEquals(availableDevice.getName().toString(),availableDevice1.getName().toString());
+        Assertions.assertNotEquals(availableDevice.getName(),availableDevice1.getName());
     }
 
+    @Test
+    public void shouldThrowDeviceEngagedExceptionWhenAllTheAvailableDevicesAreLocked(){
+        List<TargetDetails> targets = new ArrayList<>();
+        for(int i=0;i<5;i++)
+            targets.add(getTargetDetails());
+
+        DeviceCache deviceCache=new DeviceCache(targets);
+        Predicate<TargetDetails> predicate = getTargetDetailsPredicate( "android");
+
+        Assertions.assertThrows(DeviceEngagedException.class,()->{
+            for(int i=0;i<8;i++) {
+                TargetDetails availableDevice = new DeviceManager(deviceCache).getAvailableDevice(predicate, true);
+            }
+        });
+    }
+
+    @Test
+    public void shouldThrowNoSuchDeviceAvailableExceptionIfThereAreNoDevices(){
+        List<TargetDetails> targets = new ArrayList<>();
+        DeviceCache deviceCache=new DeviceCache(targets);
+        Predicate<TargetDetails> predicate = getTargetDetailsPredicate( "android");
+        Assertions.assertThrows(NoSuchDeviceException.class,()->new DeviceManager(deviceCache).getAvailableDevice(predicate));
+    }
+
+    @Test
+    public void shouldLockAndUnLockTheDevicesWhenSpecified(){
+        TargetDetails pixel3 =
+                TargetDetails.builder()
+                        .platform(EkamSupportedPlatforms.ANDROID)
+                        .name("Google Pixel 3")
+                        .udid("321")
+                        .build();
+        TargetDetails pixel4 =
+                TargetDetails.builder()
+                        .platform(EkamSupportedPlatforms.ANDROID)
+                        .name("Google Pixel 4")
+                        .udid("236")
+                        .build();
+        List<TargetDetails> targets =List.of(pixel3,pixel4);
+
+        DeviceCache deviceCache=new DeviceCache(targets);
+        DeviceManager deviceManager=new DeviceManager(deviceCache);
+        Predicate<TargetDetails> predicate = getTargetDetailsPredicate( "Google","android");
+
+        deviceCache.lock("321");
+        deviceCache.lock("236");
+
+        Assertions.assertFalse(deviceCache.isAvailable(predicate));
+        deviceManager.releaseDevice(pixel3);
+        Assertions.assertTrue(deviceCache.isAvailable(predicate));
+    }
+
+    private TargetDetails getTargetDetails() {
+        TargetDetails pixel =
+                TargetDetails.builder()
+                        .platform(EkamSupportedPlatforms.ANDROID)
+                        .name("Google Pixel "+UUID.randomUUID().toString())
+                        .udid(UUID.randomUUID().toString())
+                        .build();
+        return pixel;
+    }
 
     private Predicate<TargetDetails> getTargetDetailsPredicate(String deviceName, String devicePlatform) {
         DeviceFilter modelFilter =
@@ -80,6 +119,13 @@ public class DeviceEngagedTests {
         DeviceFilters filters =
                 DeviceFilters.builder()
                         .model(modelFilter)
+                        .build();
+        return new DeviceFiltersManager().createDeviceFilters(devicePlatform,filters);
+    }
+
+    private Predicate<TargetDetails> getTargetDetailsPredicate(String devicePlatform) {
+        DeviceFilters filters =
+                DeviceFilters.builder()
                         .build();
         return new DeviceFiltersManager().createDeviceFilters(devicePlatform,filters);
     }
